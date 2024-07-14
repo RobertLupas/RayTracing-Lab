9e87653a18fdf9a93e56ea28fb19d0e9daab8c73
Robert Lupas <robertlupas09@gmail.com>
2024-07-15 02:21:50 +0300

    Add anti-aliasing

--- color.h
+++ color.h
@@ -1,6 +1,7 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "interval.h"
 #include "vec3.h"
 
 using color = vec3;
@@ -11,9 +12,10 @@ void write_color(unsigned char* buffer, int index, const color& pixel_color) {
     auto b = pixel_color.z();
 
     // Translate the [0,1] component values to the byte range [0,255].
-    int rbyte = static_cast<int>(255.999 * r);
-    int gbyte = static_cast<int>(255.999 * g);
-    int bbyte = static_cast<int>(255.999 * b);
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
 
     // Write the color to the buffer at the specified index.
     buffer[index] = rbyte;
