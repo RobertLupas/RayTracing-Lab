719e4303106783650762451b737e763034436b59
Robert Lupas <robertlupas09@gmail.com>
2024-07-15 16:44:31 +0300

    Add depth of field

--- RayTracing in One Weekend.cpp
+++ RayTracing in One Weekend.cpp
@@ -31,10 +31,13 @@ int main()
     cam.samples_per_pixel = 200;
     cam.max_depth = 50;
 
-    cam.vfov = 70;
+    cam.vfov = 20;
     cam.lookfrom = point3(-2, 2, 1);
     cam.lookat = point3(0, 0, -1);
     cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 10.0;
+    cam.focus_dist = 3.4;
 
     cam.render(world);
 
